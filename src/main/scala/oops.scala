1cf8da4cd9184d2b851bc3e2b946b0a54756c741
ParasJain-knoluds <paras.jain@knoldus.com>
2021-08-06 12:45:32 +0530

    modifiy oops.scala file

--- src/main/scala/oops.scala
+++ src/main/scala/oops.scala
@@ -1,23 +1,27 @@
 
-class Person(var name:String,var age:Int) extends Ordered[Person] {
+class Person(var name: String, var age: Int) extends Ordered[Person] {
   def compare(that: Person): Int = {
     this.name compare that.name match {
-      case 0 => { if(this.age.equals(that.age))
-        println("True\nName & age both are same")
-      else
-        println("False\nName are same but ages are not equal");
+      case 0 => {
+        if (this.age.equals(that.age))
+          println("True\nNames & age both are same")
+        else
+          println("False\nNames are same but age are not same");
         0
       }
-      case other => { if(this.name.length.equals(that.name.length) && this.age.equals(that.age))
-        println("True");
-      else println("false");
+      case other => {
+        if (this.name.length.equals(that.name.length) && this.age.equals(that.age))
+          println("True");
+        else
+          println("False");
         1
       }
     }
   }
 }
-object Person extends  App{
-  var personOne = new Person("Paras",22)
-  var personTwo = new Person("Paras",22)
+
+object Person extends App {
+  var personOne = new Person("Paras", 22)
+  var personTwo = new Person("Paras", 22)
   personOne compare personTwo
 }
