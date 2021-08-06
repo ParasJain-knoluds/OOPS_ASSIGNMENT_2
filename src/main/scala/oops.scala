4dd2eb52c6c07dd8511fca3eb199a86235f0cd68
ParasJain-knoluds <paras.jain@knoldus.com>
2021-08-06 11:51:45 +0530

    modifies the oops.scala file

--- src/main/scala/oops.scala
+++ src/main/scala/oops.scala
@@ -1,18 +1,28 @@
 trait  Ordered{
   def check(p: Person)
+
 }
 
 class Person(val name: String, val age: Int) extends Ordered {
   def check(p: Person) {
-
     if(this.name.equals(p.name)){
-      if(this.age.equals(p.age))
-        println("Persons Names and Age of both are same")
-      else
-        println("Persons Names are same but Age is not same")
+      if (this.age equals(p.age)){
+        println("true | Persons Names and Age both are same");
+      }
+      else{
+        println("False | Persons Names is same but Age is not same");
+      }
     }
-    else
-      println("Persons Names is not same")
+    else{
+      if (this.name.length equals(p.name.length)) {
+        println("true | Persons Name length are same");
+      }
+      else
+      {
+        println("False | Persons Name length are not same");
+      }
+
+    }
   }
 }
 
@@ -21,8 +31,7 @@ object Oops {
   def main(args: Array[String]) {
 
     var p1 = new Person("Paras", 22)
-    var p2 = new Person("Paras", 23)
-    //  println(p1.name)
+    var p2 = new Person("Paras", 22)
     p1.check(p2)
 
 
